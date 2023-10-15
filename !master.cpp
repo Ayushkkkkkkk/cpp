2fbc7e8058a991a098e26903ff77e93a5a1c88eb
Ayush_sccg <ayush1240945@gmail.com>
2023-10-15 12:21:48 +0545

    pheonix and socks

--- !master.cpp
+++ !master.cpp
@@ -131,25 +131,49 @@ template<typename T, typename U> void umax(T& a, U b) {if (a < b) a = b;}
 #define FOR(...) F_ORC(__VA_ARGS__)(__VA_ARGS__)
 #define EACH(x, a) for (auto& x: a)
 
+int N,L,R;
+int C[200001];
+int lcnt[200001],rcnt[200001];
 
 
 void test() {
-	int n , m , x ; cin >> n >> m >> x;
-	int h[n];
-	cout << "YES" << nline;
-	set<pair<int , int>> s;
-	for (int i = 1 ; i <= m ; i++){
-		s.insert({0 , i});
-	}
-	for (int i = 0 ; i < n ; i++){
-		cin >> h[i];
-		pair<int , int> p = *s.begin();
-		s.erase(p);
-		cout << p.second << ' ';
-		s.insert({p.first + h[i] , p.second});
-	}
-	cout << nline;
-	
+	cin>>N>>L>>R;
+  for (int i=1;i<=N;i++){
+    lcnt[i]=0;
+    rcnt[i]=0;
+  }
+  for (int i=1;i<=N;i++){
+    cin>>C[i];
+    if (i<=L)
+      lcnt[C[i]]++;
+    else
+      rcnt[C[i]]++;
+  }
+  //remove pairs that are already matching
+  for (int i=1;i<=N;i++){
+    int mn=min(lcnt[i],rcnt[i]);
+    lcnt[i]-=mn;
+    rcnt[i]-=mn;
+    L-=mn;
+    R-=mn;
+  }
+  if (L<R){
+    swap(lcnt,rcnt);
+    swap(L,R);
+  }
+  //now, there are at least as many left socks as right socks
+  int ans=0;
+  for (int i=1;i<=N;i++){
+    int extra=L-R; //always even
+    int canDo=lcnt[i]/2;
+    int Do=min(canDo*2,extra);
+    //turn "Do"/2 left socks of color i into right socks
+    ans+=Do/2;
+    L-=Do;
+  }
+  //turn extra lefts into rights, then adjust all colors
+  ans+=(L-R)/2+(L+R)/2;
+  cout<<ans<<endl;
 }
 
 
