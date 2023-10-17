80274e4945d5c81aea8dcb9449c4db6bc816aef9
Ayush_sccg <ayush1240945@gmail.com>
2023-10-17 07:48:24 +0545

    xor

--- !master.cpp
+++ !master.cpp
@@ -131,49 +131,22 @@ template<typename T, typename U> void umax(T& a, U b) {if (a < b) a = b;}
 #define FOR(...) F_ORC(__VA_ARGS__)(__VA_ARGS__)
 #define EACH(x, a) for (auto& x: a)
 
-int N,L,R;
-int C[200001];
-int lcnt[200001],rcnt[200001];
 
 
 void test() {
-	cin>>N>>L>>R;
-  for (int i=1;i<=N;i++){
-    lcnt[i]=0;
-    rcnt[i]=0;
-  }
-  for (int i=1;i<=N;i++){
-    cin>>C[i];
-    if (i<=L)
-      lcnt[C[i]]++;
-    else
-      rcnt[C[i]]++;
-  }
-  //remove pairs that are already matching
-  for (int i=1;i<=N;i++){
-    int mn=min(lcnt[i],rcnt[i]);
-    lcnt[i]-=mn;
-    rcnt[i]-=mn;
-    L-=mn;
-    R-=mn;
-  }
-  if (L<R){
-    swap(lcnt,rcnt);
-    swap(L,R);
-  }
-  //now, there are at least as many left socks as right socks
-  int ans=0;
-  for (int i=1;i<=N;i++){
-    int extra=L-R; //always even
-    int canDo=lcnt[i]/2;
-    int Do=min(canDo*2,extra);
-    //turn "Do"/2 left socks of color i into right socks
-    ans+=Do/2;
-    L-=Do;
-  }
-  //turn extra lefts into rights, then adjust all colors
-  ans+=(L-R)/2+(L+R)/2;
-  cout<<ans<<endl;
+	int n ; cin >> n ;
+	int pref[2005];
+	for (int i = 1 ; i <= n ; i++){
+		int a ; cin >> a;
+		pref[i] = (pref[i - 1] ^ a);
+	}
+	bool yes = !pref[n];
+	for (int i = 1 ; i <= n ; i++){
+		for (int  j = i + 1 ; j < n ; j++){
+			yes |= (pref[i] == (pref[j] ^ pref[i]) && pref[i] == (pref[n] ^ pref[j]));
+		}
+	}
+	puts(yes ? "YES" : "NO");
 }
 
 
