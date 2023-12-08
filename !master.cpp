fb80a70843bf1f7ab80c0291fd6dd23c537e7718
Ayush_sccg <ayush1240945@gmail.com>
2023-12-08 07:55:43 +0545

    chess

--- !master.cpp
+++ !master.cpp
@@ -131,22 +131,84 @@ template<typename T, typename U> void umax(T& a, U b) {if (a < b) a = b;}
 #define FOR(...) F_ORC(__VA_ARGS__)(__VA_ARGS__)
 #define EACH(x, a) for (auto& x: a)
 
+#define  _CRT_SECURE_NO_WARNINGS
+
+
+
+#define INF 0x3f3f3f3f3f3f3f3fLL
+#define watch(x) cout<<(#x)<<" = "<<x<<endl
+const int dr[]{ -1, -1, 0, 1, 1, 1, 0, -1 };
+const int dc[]{ 0, 1, 1, 1, 0, -1, -1, -1 };
+#if __cplusplus >= 201402L
+template<typename T>
+vector<T> create(size_t n) {
+	return vector<T>(n);
+}
+template<typename T, typename ... Args>
+auto create(size_t n, Args ... args) {
+	return vector<decltype(create<T>(args...))>(n, create<T>(args...));
+}
+#endif
+void run() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+	//freopen("input.txt", "r", stdin);
+#else
+#endif
+}
+ 
+const int N = 8, LIMIT_QUERIES = 130 + 1;
+int curY = 0, cnt = 0;
+string Move(int x, int y) {
+	assert(++cnt <= LIMIT_QUERIES);
+	cout << x << ' ' << y << endl;
+	curY = y;
+	string s;
+	cin >> s;
+	return s;
+}
+ 
+bool scanRow(int row) {
+	string s;
+	if (curY == 8) {
+		for (int y = 7; y > 0; y--) {
+			s = Move(row, y);
+			if (s == "Done")
+				return true;
+			if (s.find("Up") != string::npos)
+				return scanRow(row);
+			if (s.find("Down") != string::npos)
+				return false;
+		}
+		return false;
+	}
+	for (int y = (curY == 1 ? 2 : 1); y <= N; y++) {
+		s = Move(row, y);
+		if (s == "Done")
+			return true;
+		if (s.find("Up") != string::npos)
+			return scanRow(row);
+		if (s.find("Down") != string::npos)
+			return false;
+	}
+	return false;
+}
+
 
 // cost = a * l + b;
-void test() {
-	int n, k;
-		cin >> n >> k;
-		vector<pair<int, int>> v(n);
-		for (int i = 0; i < n; i++) {
-			cin >> v[i].first;
-			v[i].second = i;
-		}
-		sort(v.begin(), v.end());
-		int ans = 1;
-		for (int i = 1; i < n; i++)
-			if (v[i - 1].second + 1 != v[i].second)
-				ans++;
-		cout << (ans <= k ? "YES" : "NO") << endl;
+bool test() {
+	curY = 1, cnt = 0;
+	string s;
+	for (int row = 1; row <= N; row++) {
+		s = Move(row, curY);
+		if (s == "Done")
+			return true;
+		if (scanRow(row))
+			return true;
+	}
+	return false;
 }
 
 
@@ -156,7 +218,8 @@ int32_t main() {
 	int t;
 	cin >> t;
 	W(t) {
-		test();
+		bool b = test();
+		assert(b);
 	}
 }
 
