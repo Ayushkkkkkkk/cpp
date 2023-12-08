2d2902dead2de24c33bb97cfc71ec95b3829c838
Ayush_sccg <ayush1240945@gmail.com>
2023-12-08 07:53:32 +0545

    subarray

--- !master.cpp
+++ !master.cpp
@@ -134,12 +134,19 @@ template<typename T, typename U> void umax(T& a, U b) {if (a < b) a = b;}
 
 // cost = a * l + b;
 void test() {
-	int n , a , b;
-	string s ;
-	cin >> n >> a >> b >> s;
-	int m = unique(s.begin() , s.end()) - s.begin();
-	debug(m);
-	cout << n * a + max(n *b , (m / 2 + 1) * b) << nline;
+	int n, k;
+		cin >> n >> k;
+		vector<pair<int, int>> v(n);
+		for (int i = 0; i < n; i++) {
+			cin >> v[i].first;
+			v[i].second = i;
+		}
+		sort(v.begin(), v.end());
+		int ans = 1;
+		for (int i = 1; i < n; i++)
+			if (v[i - 1].second + 1 != v[i].second)
+				ans++;
+		cout << (ans <= k ? "YES" : "NO") << endl;
 }
 
 
